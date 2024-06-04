45c713752734ece7b8529b468472a8b8b441c5df
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-06-04 12:40:34 +0300

    make build

---
+++ index.ede47a13.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");if(r){var l=r.getAttribute("href");t.setAttribute("src",l)}});
+//# sourceMappingURL=index.ede47a13.js.map
